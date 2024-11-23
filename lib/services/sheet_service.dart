bdf3273f191da66baeda9ff29d417212f023865e
naimulhassan <developernaimul00@gmail.com>
2024-11-23 17:08:45 +0600

    improve code

--- lib/services/sheet_service.dart
+++ lib/services/sheet_service.dart
@@ -5,17 +5,16 @@ class SheetsApi {
   static const credentials = r'''{
   "type": "service_account",
   "project_id": "healthy-kayak-442604-h3",
-  "private_key_id": "48532c8ad97b8dc166f136132760673a1918a4d5",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCm9EAQR095SttQ\nCE7Y2pVabpCf/ynpcHmD37vFDqzE0QJvEIeRutRa6cTcbhhyqYXBVz2W+Arw9dOq\nxY2fAyqVn+6EG2aCpBfLJYpE+chtGAu3wic04vEk5hzN/UuO0V8jMxCuwuHCoWzv\nCrHudRiMyEBSBQsz2Rq0vCNFk0M5ehSIrnmTmDI6aEIqZf6GdcLU84iqw+pXFwkW\nj3nEisz3CH/RKHvqQpZoMIPygUueN0fcgmolxt/OoVBfEYKOk/rHtdFTU/tNQfrD\nl1O8/dtcqEMT9gG4X1aMu0scwMapVwk+aGzUmPQYeMfffeZ2IBGIYncyMKNjVX+j\nsWQf0LQ9AgMBAAECggEADDZR7f7msNF0G7Vn3b6WGIXS6BPYQyjvh+EYkTsCvJN/\nUT7NHoX4h5KRoarJ+K/ZcVbffJ4LNYH90tWIMklJ1vpaY5PE2qnZywnQGeiIK8wL\npgw6evNeE/FjH6w3V9c31XwkQcGj6u03lIAHRjvkKP7IOa0AcJv2XfcPFhqbWxD/\nxfgsSp51kQLuNKV8EPlQD+SBXqVEvLAI920vFRpR3UMZ+F4YCX71aAHCNXva0KR4\ncBnTMoL2RQ1tpIH238Ido5hpZ5YIFFP2os0eBzTALi59H1WTZOtIPW1dtB19ZxIz\nmeBVrs0LZUbeho4qXlvQU3/5WdLb1LgdmorVKaFuoQKBgQDkBQnh25Y1qyITvQZh\nrXdWJeTQP0Tv8auoixjalkSX4VWxV3CGrl2LbRRkQau96tWQLBnmzhhyHmzlwfQk\nRzfg40HWS40iwrombO8EU2f9+Kk1Fm+o4cwd1w4BAWOqxhLzGSyDD1jcWA7lJue5\nlcesyRFODjJnmeUUtSOW4gse8QKBgQC7cOkev29e2z0S2ZUVV6kqmxKazzLuXYKk\nq38ac+XWH3I2oPKiS8Oi5LnDgY1h9K1STTxXtoPDWmdp7owF/lMCr9csuJN//YwT\nqwBO01eKul+yKEe/bHAubXHz8+g6m8FU4THKU8txjDCvVaiBT/swOkJL1gGUqU2L\n+q33J6ZCDQKBgQDEagayNFn2zvxirlAB8c+7OJcaP3XdjnqEi5UULZ7WibcsNRrs\nGOThhkurdNpAlTnzn9mL9HogS08YtvLcETD0Gi6LLrdbewuor+tP+Zy5q+ntne7/\nNcmC5s3cVdsJrueB2mx6VM0BuSTThajUN04x611ptFb84RbwuHv4q6xSsQKBgHu9\nyZhr5tknQQVtsNb7HQmsgDbFLKliADo3oVxUQNn/voXD8QolnEywVNZ1c2HEhpt7\ns6dwLUJNL1ImkjfustxmO1Yhr/Zh6xSc4kInkXZbHYvevaborpJn3D+huKDgbFxd\nomIguYEHfKhzkx9zLq+i9ltcN4Tt0+ziG/C7My4NAoGAKUaGeraQ5SnXYXWNffbU\nzb9X8DsnqOPhlpSIgN1pjFiukQ3xLDVORbXsdmS2ERzjSN7lIAP31knkUv+8pSxr\nvwKgwuiXx0XaxUfoN9tus3Hf1XaCd/8XwFMOe1I7aeSTelmFiupVIADimFjnawEc\nWVhGRX74opKLJurJxYOROO4=\n-----END PRIVATE KEY-----\n",
-  "client_email": "google-sheet@healthy-kayak-442604-h3.iam.gserviceaccount.com",
-  "client_id": "118056912411566717715",
+  "private_key_id": "c7f5c0e8318967018167b2df0df01723552da7b0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDOMyIoRycQwg/s\nBYVxyoKET58CEQacOYU6J7TVmtCRSlNF4ZVWZfv02AMJi9l3+pIyEUurKetcO//V\npSJJcDNhwF/GsQU+tj3SnFGWBV/2on6YZ40iUL5o7zf3Y5hHbABC0UZA6Z3enAX3\nNu30XVYqrnzl4PP0z+W5/gCcPvh+GaKMa+ZPWU+pd2E2zsZV+99YLKq0e+GoepRF\n/DjI+tBhznRh9i2DLcKdXsZRz3vX5eE+jC4lj8KYQQxUTJn/f1iOHYUetncqXLSn\n6bLadZGUcPaeXUy/oCphh/XPD+l8FTa0PNpdMC1QPvd8vCQh3eejOF1+Egs/sqUB\nhsc8GKCbAgMBAAECggEAE1RQxeTRoS0H8BCeb0nUDVInbjqpW3i65/Yz6fguyAfn\nIXxhku1RfqTHOBxfgPNAxCfB56Qw138aXu0xdx/o+Sde8xHCE5xi1DzqGv2a/tRB\nuMiwPsDB8LVIEk1IR520so7Knqgnr+gBWSjRiqA7RRoBRE1foRniB5rrdLokcjGK\n4VDNq6/BE0ZtRW4xsSolnHgBC4O5Lm7/7PrtxRSpwI+e0u2OQDmXatdMPDR1zyww\nzjUPnLhQygnUf4AGJpAxE6tVeZYvEVyOIEIpnPHsc9aq7iO3q8TyhG9sdibtsodd\nEinfsNz/oa+2aQGeGmol7z7iMfZjmfHn0yBie3oTXQKBgQDv1g0uWLoNw5m0XNcU\ncg9alEAJkpp22StrgQo42YChrhujWbsMAg8YQzQzGTLDKywF+SjqQU7XCHp0zLVI\nCrcJJFG5KD7xfe3Xy2/x8pObYgQWfCBPT3GlT8gE3P9uqmBYYKGj4OtRvJNJOfYq\nNmIiLq8T3kaT//bVSq5IBJE1DQKBgQDcGL7PceKggjDowhI7zaMa9dHFwnHmZYsG\nLxsYyo92Qsx9dfleOg7d1qA02/NfqXR0uaxyAjel7+H9C4R/9M9TZgdWVN0gzy5G\nUKkYmI/q6g9rUdxoHTYyK/EdvNs1+2UWRHt8td60K4ssjbyTOQAJ0cKAx+tnO9ZS\nuTuldHgSRwKBgDFiu04a2RzydRkNsQA1yHGXa2RNOt9UMrutU6SHLRSd3HUTig+b\n8O3HEN2gx7ImlqcYOlOaZRikV0rryDxwcGZCg02EOwnRK4OvEXsZv0lUspDKuIB8\nVGN8/Sv7MeHg1Xv9UeuD6hDnqtB+TfE9R5HQWR7XO2NIZtRez2pm/luVAoGAKtGt\nnZIccvmhJkdLYNMa3k3gp8ayfg5XQ7a6YNJXorxQKEBhaVGGPuZYeLAp4vyGqYbN\n48hGp0PKBX7OIk6wXxtEZilQc+eaqxdaLmzP4vXyVgCxDphQSOZ/nDd3tkLYYcKk\n+5guYYLvKpXQkBvQxBhI7OD4DTd49NMQpZnJGlUCgYAb4n+oG+fUjr68Cye+YH5q\noK5TWkkB028xzlQNpqGei+Ux+wQhiEtrWED6LsZmfnwEHFS0FVXj3bK/pozmdSMV\nGpSHxyHKEp31VINylbfSTFUsBpM8S/K/jD1QWYoPGRiF1YfiOdXPXVsVU5Cng7H8\nzBMQOiuEmloznlAKTVHPEw==\n-----END PRIVATE KEY-----\n",
+  "client_email": "google-sheets@healthy-kayak-442604-h3.iam.gserviceaccount.com",
+  "client_id": "114131269673329969336",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/google-sheet%40healthy-kayak-442604-h3.iam.gserviceaccount.com",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/google-sheets%40healthy-kayak-442604-h3.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
-}
-''';
+}''';
 
   static const sheetId = "1gJ0qc63AX24KrDRspy5myp3uJpNVzVDx7R9dkX5otNU";
 
